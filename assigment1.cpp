a3fd23ac0fa21240575489914d0371263820a24a
Saumitra Upadhyay <142289554+Saumitra28@users.noreply.github.com>
2023-10-26 19:51:20 +0530

    Assignment 1

---
+++ assigment1.cpp
@@ -0,0 +1,127 @@
+// #include <stdio.h>
+
+// int main() {
+//     printf("Hello Students\n");
+//     return 0;
+// }
+//Ques 1 ends
+// #include <stdio.h>
+
+
+// int main() {
+//     printf("Hello\n");
+//     printf("Students\n");
+//     return 0;
+// }
+// Ques 2 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     printf("\"MySirG\"\n");
+//     return 0;
+// }
+// Ques 3 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     double radius, area;
+//     printf("Enter the radius of the circle: ");
+//     scanf("%lf", &radius);
+    
+//     area = 3.14159265359 * radius * radius;
+    
+//     printf("Area of circle is %.2f having the radius %.2f\n", area, radius);
+//     return 0;
+// }
+//ques 4 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     double radius, area;
+//     printf("Enter the radius of the circle: ");
+//     scanf("%lf", &radius);
+    
+//     area = 3.14159265359 * radius * radius;
+    
+//     printf("Area of circle is %.2f having the radius %.2f\n", area, radius);
+//     return 0;
+// }
+//ques 5 ends
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+//     char str[] = "Hello, World!";
+//     int length = strlen(str);
+    
+//     printf("Length of the string is %d\n", length);
+//     return 0;
+// }
+//ques 6 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     char name[] = "Amit Kumar";
+    
+//     printf("\"Hello, %s\"\n", name);
+//     return 0;
+// }
+// ques 7 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     printf("%%d\n");
+//     return 0;
+// }
+//ques 8 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     printf("\\n\n");
+//     return 0;
+// }
+// ques 9 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     printf("\\\\\n");
+//     return 0;
+// }
+// ques 10 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     int day, month, year;
+//     printf("Enter a date in DD/MM/YYYY format: ");
+//     scanf("%d/%d/%d", &day, &month, &year);
+    
+//     printf("Day - %02d , Month - %02d , Year - %04d\n", day, month, year);
+//     return 0;
+// }
+//ques 11 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     int hours, minutes;
+//     printf("Enter a time in HH:MM format: ");
+//     scanf("%d:%d", &hours, &minutes);
+    
+//     printf("%02d Hour and %02d Minute\n", hours, minutes);
+//     return 0;
+// }
+
+//output: ineuron7
+
+
+
+
