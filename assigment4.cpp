d94d31a8bf3d304062bca08a78ea44f14015dce9
Saumitra Upadhyay <142289554+Saumitra28@users.noreply.github.com>
2023-10-26 20:27:25 +0530

    Assignment 4

---
+++ assigment4.cpp
@@ -0,0 +1,118 @@
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 0; i < 5; i++) {
+//         printf("MySirG\n");
+//     }
+//     return 0;
+// }
+
+//ques 1 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 1; i <= 10; i++) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+//ques 2 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 10; i >= 1; i--) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+//ques3 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 1; i <= 20; i += 2) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+//ques 4 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 19; i >= 1; i -= 2) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+
+//ques 5 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 2; i <= 20; i += 2) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+//ques 6 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 20; i >= 2; i -= 2) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+
+//ques 7 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 1; i <= 10; i++) {
+//         printf("Square of %d: %d\n", i, i * i);
+//     }
+//     return 0;
+// }
+
+//ques 8 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     for (int i = 1; i <= 10; i++) {
+//         printf("Cube of %d: %d\n", i, i * i * i);
+//     }
+//     return 0;
+// }
+
+//ques 9 ends
+
+// #include <stdio.h>
+
+// int main() {
+//     int number = 5;
+//     printf("Table of 5:\n");
+//     for (int i = 1; i <= 10; i++) {
+//         printf("%d x %d = %d\n", number, i, number * i);
+//     }
+//     return 0;
+// }
+//ques 10 ends
