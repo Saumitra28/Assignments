cb0ab24039cd1be4757f5b3516e1bcb5d0f11099
Saumitra Upadhyay <142289554+Saumitra28@users.noreply.github.com>
2023-10-26 20:33:47 +0530

    Add files via upload

---
+++ assigment5.cpp
@@ -0,0 +1,146 @@
+// #include <stdio.h>
+
+// int main() {
+//   int N;
+// printf("Enter the value of N: ");
+// scanf("%d", &N);
+//     for (int i = 0; i < N; i++) {
+//         printf("MySirG\n");
+//     }
+//     return 0;
+// }
+
+// ques 1 ends
+
+// #include <stdio.h>
+
+// int main() {
+//   int N;
+//     printf("Enter the value of N: ");
+//     scanf("%d", &N);
+//     for (int i = 1; i <= N; i++) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// ques 2 ends
+
+// #include <stdio.h>
+
+// int main() {
+//   int N;
+//     printf("Enter the value of N: ");
+//     scanf("%d", &N);
+//     for (int i = N; i >= 1; i--) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// ques3 ends
+
+// #include <stdio.h>
+
+// int main() {
+//   int N;
+//     printf("Enter the value of N: ");
+//     scanf("%d", &N);
+//     for (int i = 1; i <= N; i += 2) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// ques 4 ends
+
+// #include <stdio.h>
+
+// int main() {
+//   int N;
+//     printf("Enter the value of N: ");
+//     scanf("%d", &N);
+//     for (int i = N; i >= 1; i -= 2) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// ques 5 ends
+
+// #include <stdio.h>
+
+// int main() {
+//   int N;
+//     printf("Enter the value of N: ");
+//     scanf("%d", &N);
+//     for (int i = 2; i <= N; i += 2) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// ques 6 ends
+
+// #include <stdio.h>
+
+// int main() {
+//   int N;
+//     printf("Enter the value of N: ");
+//     scanf("%d", &N);
+//     for (int i = N; i >= 2; i -= 2) {
+//         printf("%d ", i);
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// ques 7 ends
+
+// #include <stdio.h>
+
+// int main() {
+//   int N;
+//     printf("Enter the value of N: ");
+//     scanf("%d", &N);
+//     for (int i = 1; i <= N; i++) {
+//         printf("Square of %d: %d\n", i, i * i);
+//     }
+//     return 0;
+// }
+
+// ques 8 ends
+
+// #include <stdio.h>
+
+// int main() {
+//   int N;
+//     printf("Enter the value of N: ");
+//     scanf("%d", &N);
+//     for (int i = 1; i <= N; i++) {
+//         printf("Cube of %d: %d\n", i, i * i * i);
+//     }
+//     return 0;
+// }
+
+// ques 9 ends
+
+// #include <stdio.h>
+
+// int main() {
+//   int N;
+//     printf("Enter the value of N: ");
+//     scanf("%d", &N);
+//     int number = 5;
+//     printf("Table of 5:\n");
+//     for (int i = 1; i <= N; i++) {
+//         printf("%d x %d = %d\n", number, i, number * i);
+//     }
+//     return 0;
+// }
+// ques 10 ends
